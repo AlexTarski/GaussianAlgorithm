def9c8a26c1f99be2b08fbd89472975d500dcfff
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-06-05 13:31:57 +0300

    Добавьте файлы проекта.

---
+++ Solver.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+namespace GaussAlgorithm;
+
+public class Solver
+{
+	public double[] Solve(double[][] matrix, double[] freeMembers)
+	{
+		throw new NotImplementedException();
+	}
+}
